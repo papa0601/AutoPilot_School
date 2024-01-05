0d994327e1c05f9af7c73fab2ec1b96f2e677e09
papa0601 <140836841+papa0601@users.noreply.github.com>
2024-01-05 11:49:32 +0900

    Added Comments

--- AutoPilot_School.ino
+++ AutoPilot_School.ino
@@ -1,34 +1,32 @@
+/* 라이브러리 불러오기 */
 #include "HUSKYLENS.h"
 #include "SoftwareSerial.h"
+
+/* 모터 드라이버와 킬 스위치 포트 지정 */
 #define DIR_L 2
 #define PWM_L 3
 #define DIR_R 4
 #define PWM_R 5
 #define BUTTON 13
 
-//#define FSPEED 100
-//#define RSPEED 60
-
+/* 기본 속도와 회전 속도 및 지연 시간 지정 */
 #define DEFAULTSPD 100
 #define EXTRASPD 40
 #define delayTime 100
 
+/* 여러 회전 상황에 대한 기준 변수 지정 */
 #define RCriteria 30
 #define ACriteria 0
 #define LOCriteria 60
 
-//void forward();
-//void stop();
-//void turnR();
-//void turnL();
-
+/* 함수 원형 선언 */
 void drive(int LSPEED, int RSPEED);
 void rear();
 
 HUSKYLENS huskylens;
 SoftwareSerial mySerial(10, 11); // RX, TX
 //HUSKYLENS green line >> Pin 10; blue line >> Pin 11
-void setup() {
+void setup() { // 기본 셋업
     Serial.begin(115200);
     mySerial.begin(9600);
     pinMode(DIR_L, OUTPUT);
@@ -46,10 +44,11 @@ void setup() {
 }
 
 void loop() {
-  int buttonState = digitalRead(BUTTON);
+  int buttonState = digitalRead(BUTTON); // 킬 스위치 상태 받아옴
+  /* 허스키 렌즈 에러에 대한 코드 */
   if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
   else if(!huskylens.isLearned()) Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
-  else if(!huskylens.available()) {
+  else if(!huskylens.available()) { // 허스키 렌즈가 선을 못 잡으면 후진 
     Serial.print(F("No block or arrow appears on the screen!"));
     Serial.println(String()+F(" 후진"));
     if (buttonState == 0)
@@ -58,31 +57,32 @@ void loop() {
   else
   {
     Serial.println(F("###########"));
+    /* 허스키 렌즈 오류 없을 때 */
     while (huskylens.available())
     {
       HUSKYLENSResult result = huskylens.read();
-      int xdelta = result.xOrigin - result.xTarget;
-      int xavg = (result.xOrigin + result.xTarget) / 2;
+      int xdelta = result.xOrigin - result.xTarget; // 화살표(선)의 시작 지점 x값 - 끝 지점 x 값 (이후 '차이'라고 한다)
+      int xavg = (result.xOrigin + result.xTarget) / 2; // 화살표(선)의 시작 지점 x값과 끝 지점 x값의 평균 (이후 '평균'이라고 한다)
       Serial.print(String()+F("Origin_axis: (")+result.xOrigin+F(", ")+result.yOrigin+F(") / Target_axis: (")+result.xTarget+F(", ")+result.yTarget+F(") ")+result.ID+F(" / xdiff: ")+xdelta);
-      if (buttonState == 0) { // 버튼 눌렀을 때
-        if (xdelta <= -RCriteria && result.xTarget >= 160 + ACriteria || xavg >= 160 + LOCriteria){ // 우측 ㄱㄱ
+      if (buttonState == 0) { // 버튼이 눌려있을 때
+        if (xdelta <= -RCriteria && result.xTarget >= 160 + ACriteria || xavg >= 160 + LOCriteria){ // 차이가 회전 변수보다 크고 화살표 방향이 진행 뱡향과 다르면 또는 선이 우측에 몰리면
           Serial.println(String()+F("우회전"));
           drive(DEFAULTSPD + EXTRASPD, DEFAULTSPD);
           //turnR();
         }
-        else if (xdelta >= RCriteria && result.xTarget <= 160 - ACriteria || xavg <= 160 - LOCriteria){ // 좌측 ㄱㄱ
+        else if (xdelta >= RCriteria && result.xTarget <= 160 - ACriteria || xavg <= 160 - LOCriteria){ // 차이가 회전 변수보다 크고 화살표 방향이 진행 뱡향과 다르면 또는 선이 좌측에 몰리면
           Serial.println(String()+F("좌회전"));
           drive(DEFAULTSPD, DEFAULTSPD + EXTRASPD);
           //turnL();
         }
-        else { // 앞으로
+        else { // 둘 다 아니면 전진
           Serial.println(String()+F("전진"));
           drive(DEFAULTSPD, DEFAULTSPD);
           //forward();
         }
       }
       
-      else {
+      else { // 허스키 렌즈가 안되면 정지 (의미 X)
         Serial.println(String()+F("정지"));
         drive(0, -17);
         //stop();
@@ -91,7 +91,7 @@ void loop() {
   }
 }
 
-void drive(int LSPEED, int RSPEED) {
+void drive(int LSPEED, int RSPEED) { // 주행 코드
   digitalWrite(DIR_L, LOW);
   digitalWrite(DIR_R, LOW);
   analogWrite(PWM_L, LSPEED);
@@ -99,44 +99,10 @@ void drive(int LSPEED, int RSPEED) {
   delay(delayTime);
 }
 
-void rear() {
+void rear() { // 후진 코드
   digitalWrite(DIR_L, HIGH);
   digitalWrite(DIR_R, HIGH);
   analogWrite(PWM_L, 80);
   analogWrite(PWM_R, 80+17);
   delay(1000);
 }
-
-/*
-void forward() {
-  digitalWrite(DIR_L, LOW); // LOW -> 전진 
-  analogWrite(PWM_L, FSPEED);
-  digitalWrite(DIR_R, LOW); // LOW -> 전진
-  analogWrite(PWM_R, FSPEED);
-  delay(delayTime);
-}
-
-void stop() {
-  analogWrite(PWM_L, 0);
-  analogWrite(PWM_R, 0);
-  delay(delayTime);
-}
-
-void turnR() {
-  digitalWrite(DIR_L, LOW); // LOW -> 전진 
-  analogWrite(PWM_L, RSPEED);
-  digitalWrite(DIR_R, HIGH); // LOW -> 전진
-  analogWrite(PWM_R, RSPEED);
-  delay(delayTime);
-}
-
-void turnL() {
-  digitalWrite(DIR_L, HIGH); // LOW -> 전진 
-  analogWrite(PWM_L, RSPEED);
-  digitalWrite(DIR_R, LOW); // LOW -> 전진
-  analogWrite(PWM_R, RSPEED);
-  delay(delayTime);
-}
-*/
-
-
